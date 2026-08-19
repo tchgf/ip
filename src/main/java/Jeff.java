4f0f6f999cc8591440a12ec5e3c0973b2f8ed06d
Fu Lingyu <fulingyu.singapore@gmail.com>
2026-08-19 17:53:50 +0800

    renamed chat bot, changed banner

---
+++ src/main/java/Jeff.java
@@ -0,0 +1,10 @@
+public class Jeff {
+    public static void main(String[] args) {
+        String banner = "     _  _____  _____  _____ \n"
+                + "    | || ____||  ___||  ___|\n"
+                + "    | || |__  | |_   | |_   \n"
+                + "| |_| ||  __| |  _|  |  _|  \n"
+                + " \\___/ |_____||_|    |_|    \n";
+        System.out.println(banner);
+    }
+}
