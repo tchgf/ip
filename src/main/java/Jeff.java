90814f1ab15677a60dc288b7248eee33e7c0f845
Fu Lingyu <fulingyu.singapore@gmail.com>
2026-08-19 17:58:22 +0800

    added greetings

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -1,10 +1,18 @@
 public class Jeff {
+    private static final String DIVIDER = "____________________________________________________________";
+
     public static void main(String[] args) {
         String banner = "     _  _____  _____  _____ \n"
                 + "    | || ____||  ___||  ___|\n"
                 + "    | || |__  | |_   | |_   \n"
                 + "| |_| ||  __| |  _|  |  _|  \n"
-                + " \\___/ |_____||_|    |_|    \n";
+                + " \\___/ |_____||_|    |_|    ";
+        System.out.println(DIVIDER);
         System.out.println(banner);
+        System.out.println("Hello! I'm Jeff.");
+        System.out.println("What can I do for you?");
+        System.out.println(DIVIDER);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
     }
 }
